185ed3d2aa1f99ccbfdf1564077b38c66defda4f
Ak-lee <laklc@126.com>
2018-01-22 20:46:06 +0800

    当用户离开时停止计时器

--- main.js
+++ main.js
@@ -1,12 +1,28 @@
 var n=1;
-var index,lastIndex
-setInterval(function(){
+var index,nextIndex
+timer=setInterval(function(){
 index=(n+5)%6+1;
-lastIndex=(n+6)%6+1;
+nextIndex=(n+6)%6+1;
 $(`.images>img:nth-child(${index})`).removeClass('current').addClass('leave')
 .one('transitionend',function(e){$(e.currentTarget).removeClass('leave').addClass('enter')});
 
-$(`.images>img:nth-child(${lastIndex})`).removeClass('enter').addClass('current');
+$(`.images>img:nth-child(${nextIndex})`).removeClass('enter').addClass('current');
 n+=1;
 }
 ,5000)
+document.addEventListener('visibilitychange',function(){
+    if(document.hidden){
+        window.clearInterval(timer)
+    }else{
+        timer=setInterval(function(){
+            index=(n+5)%6+1;
+            nextIndex=(n+6)%6+1;
+            $(`.images>img:nth-child(${index})`).removeClass('current').addClass('leave')
+            .one('transitionend',function(e){$(e.currentTarget).removeClass('leave').addClass('enter')});
+            
+            $(`.images>img:nth-child(${nextIndex})`).removeClass('enter').addClass('current');
+            n+=1;
+            }
+            ,5000)
+    }
+})
